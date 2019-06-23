0d02baedf912452345a90ad6a384b8ce9ff553f4
Mousa Moradi <mousamk@gmail.com>
2019-06-24 01:03:40 +0430

    Show movies in the movies fragment.

--- app/src/main/java/pro/mousa/cleanmovies/features/movies/MoviesFragment.kt
+++ app/src/main/java/pro/mousa/cleanmovies/features/movies/MoviesFragment.kt
@@ -1,25 +1,77 @@
 package pro.mousa.cleanmovies.features.movies
 
 import android.os.Bundle
+import android.support.annotation.StringRes
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.View
+import kotlinx.android.synthetic.main.fragment_movies.*
 import pro.mousa.cleanmovies.R
+import pro.mousa.cleanmovies.core.exception.Failure
+import pro.mousa.cleanmovies.core.exception.Failure.*
+import pro.mousa.cleanmovies.core.extension.invisible
+import pro.mousa.cleanmovies.core.extension.observe
+import pro.mousa.cleanmovies.core.extension.failure
+import pro.mousa.cleanmovies.core.extension.viewModel
+import pro.mousa.cleanmovies.core.extension.visible
 import pro.mousa.cleanmovies.core.navigation.Navigator
 import pro.mousa.cleanmovies.core.platform.BaseFragment
+import pro.mousa.cleanmovies.features.movies.MovieFailure.*
 import javax.inject.Inject
 
 class MoviesFragment : BaseFragment() {
 
     @Inject lateinit var navigator: Navigator
+    @Inject lateinit var moviesAdapter: MoviesAdapter
+
+    private lateinit var moviesViewModel: MoviesViewModel
 
     override fun layoutId() = R.layout.fragment_movies
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         appComponent.inject(this)
+        moviesViewModel = viewModel(viewModelFactory) {
+            observe(movies, ::renderMoviesList)
+            failure(failure, ::handleFailure)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initializeView()
+        loadMoviesList()
+    }
+
+    private fun initializeView() {
+        movieList.layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        movieList.adapter = moviesAdapter
+    }
+
+    private fun loadMoviesList() {
+        emptyView.invisible()
+        movieList.visible()
+        showProgress()
+        moviesViewModel.loadMovies()
+    }
+
+    private fun renderMoviesList(movies: List<MovieView>?) {
+        moviesAdapter.collection = movies.orEmpty()
+        hideProgress()
+    }
+
+    private fun handleFailure(failure: Failure?) {
+        when(failure) {
+            is NetworkConnection -> renderFailure(R.string.failure_network_connection)
+            is ServerError -> renderFailure(R.string.failure_server_error)
+            is ListNotAvailable -> renderFailure(R.string.failure_movie_list_unavailable)
+        }
+    }
+
+    private fun renderFailure(@StringRes message: Int) {
+        movieList.invisible()
+        emptyView.visible()
+        hideProgress()
+        notifyWithAction(message, R.string.action_refresh, ::loadMoviesList)
     }
 
 }
