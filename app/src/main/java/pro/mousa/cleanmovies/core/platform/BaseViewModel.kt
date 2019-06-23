4d8133be16c8d1f40fd30387a50113aa4b7ef12f
Mousa Moradi <mousamk@gmail.com>
2019-06-24 01:02:47 +0430

    Add helper method to base view model.

--- app/src/main/java/pro/mousa/cleanmovies/core/platform/BaseViewModel.kt
+++ app/src/main/java/pro/mousa/cleanmovies/core/platform/BaseViewModel.kt
@@ -1,6 +1,15 @@
 package pro.mousa.cleanmovies.core.platform
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import pro.mousa.cleanmovies.core.exception.Failure
 
 abstract class BaseViewModel : ViewModel() {
+
+    var failure: MutableLiveData<Failure> = MutableLiveData()
+
+    protected fun handleFailure(failure: Failure) {
+        this.failure.value = failure
+    }
+
 }
