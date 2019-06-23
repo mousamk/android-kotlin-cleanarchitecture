a11f7997d4ec4cffe0ae2d45670dd4bff7b74310
Mousa Moradi <mousamk@gmail.com>
2019-06-24 01:03:18 +0430

    Add provider methods.

--- app/src/main/java/pro/mousa/cleanmovies/core/di/ApplicationModule.kt
+++ app/src/main/java/pro/mousa/cleanmovies/core/di/ApplicationModule.kt
@@ -3,7 +3,13 @@ package pro.mousa.cleanmovies.core.di
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import pro.mousa.cleanmovies.AndroidApplication
+import pro.mousa.cleanmovies.BuildConfig
+import pro.mousa.cleanmovies.features.movies.MoviesRepository
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -12,4 +18,27 @@ class ApplicationModule(private val application: AndroidApplication) {
     @Provides
     @Singleton
     fun provideApplicationContext(): Context = application
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture-Kotlin/")
+            .client(createClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun createClient(): OkHttpClient {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+            okHttpClientBuilder.addInterceptor(loggingInterceptor)
+        }
+        return okHttpClientBuilder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(dataSource: MoviesRepository.Network): MoviesRepository = dataSource
 }
