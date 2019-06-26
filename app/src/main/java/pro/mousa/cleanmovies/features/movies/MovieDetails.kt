e42fd384e5c8746e4a13e8e4b522e58085d04073
Mousa Moradi <mousamk@gmail.com>
2019-06-27 01:07:21 +0430

    Add new entities.

---
+++ app/src/main/java/pro/mousa/cleanmovies/features/movies/MovieDetails.kt
@@ -0,0 +1,20 @@
+package pro.mousa.cleanmovies.features.movies
+
+import pro.mousa.cleanmovies.core.extension.empty
+
+class MovieDetails(
+    val id: Int,
+    val title: String,
+    val poster: String,
+    val summary: String,
+    val cast: String,
+    val director: String,
+    val year: Int,
+    val trailer: String
+) {
+
+    companion object {
+        fun empty() = MovieDetails(0, String.empty(), String.empty(), String.empty(), String.empty(),
+            String.empty(), 0, String.empty())
+    }
+}
